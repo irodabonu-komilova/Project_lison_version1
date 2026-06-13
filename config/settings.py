0df42decbeb189e9a9ce4cf696f61c9951e9f9e9
SardorToirov <toirovsardor62@gmail.com>
2026-06-14 00:01:59 +0500

    auth app create

--- config/settings.py
+++ config/settings.py
@@ -16,11 +16,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lison'
+    'lison',
+    'auth_app'
 
 
 ]
-
 
 
 MIDDLEWARE = [
